42436657d88ec83f1f38d311b99703832f4e90bf
krishnapanisai <64000863+krishnapanisai@users.noreply.github.com>
2020-04-21 12:55:59 +0530

    Add files via upload

---
+++ Loop1.java
@@ -0,0 +1,31 @@
+class A{
+
+	public static void main(String[] args) {
+		
+		/* loop is flow control statement which is used to 
+		exe a block of codes in a repaptative manner based on 
+		the programmers requirement */
+
+       //Loops help in reducing repaptive lines of codes.
+       //Loops help in memory management
+
+       /*----- for loop ---------
+       --------syntax------
+       for(;;){
+
+	    block of codes...
+
+       }*/  //3 times
+ 
+           
+             for ( int i = 1;i<4; i = i + 1) {              		System.out.println(i);
+             	   System.out.println("Bunch of Morons");
+             }
+            
+             //iteration
+             //iteration tracking
+
+             
+
+	}
+}
